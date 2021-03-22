ca0077dd3226c21bfb55755bba4ce7cbfac500ef
Utshab-Das <utshab.aiub@gmail.com>
2021-03-22 19:53:32 +0600

    Final lab task 2 Bank

---
+++ BankProSln/BankPro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Account("Soumuya", "2021", 500000);
+            var s1 = new Account("Athoy ", "2014", 4000000);
+            p1.Transfer(57000, s1);
+            p1.showAllTransactions();
+            s1.showAllTransactions();
+        }
+    }
+}
---
+++ BankProSln/BankPro/Transaction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankPro
+{
+    class Transaction
+    {
+        public Account sender 
+
+        { get; set; }
+        public Account reciever 
+
+        { get; set; }
+        public int amount 
+
+        { get; set; }
+        public string additionalInfo
+        
+        { get; set; }
+        public Transaction() { }
+        public Transaction(Account sender, Account reciever, int amount, string additionalInfo)
+            
+        {
+            this.sender = sender;
+            this.reciever = reciever;
+            this.amount = amount;
+            this.additionalInfo = additionalInfo;
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Sender Name: {0} Account ID: {1}", sender.accName, sender.accId);
+            Console.WriteLine("Receiver Name:{0} Account ID:{1}", reciever.accName, reciever.accId);
+            Console.WriteLine("Ammount:" + amount);
+            Console.WriteLine("Additional Information:" + additionalInfo);
+            Console.WriteLine();
+        }
+    }
+    }
+
